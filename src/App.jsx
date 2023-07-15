5524e6bf342274f42ac19f8d0e247d2a9f450205
milon roy <milonchandro35@gmail.com>
2023-07-15 19:30:40 +0600

    first time comment

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+import Header from "./components/Header"
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Form></Form>
+    </div>
+  );
+};
+
+export default App;
